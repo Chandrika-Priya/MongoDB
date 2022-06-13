d4936769530fc1d0c3c39346127cc69040f0d022
Chandrika-Priya <chandrika.bogadi@gmail.com>
2022-06-13 07:08:53 +0530

    [Priya] initial commit.

---
+++ src/test/java/com/example/mongoldb/MongoldbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongoldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoldbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
